0ad4b0541f1638a315d06e1f5441430a83266dfd
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-06-14 18:51:36 -0700

    Refactor interviewer components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -9,8 +9,8 @@ export default function InterviewerList(props) {
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
+       
         {props.interviewers.map((interviewer) => {
-
           return (
             <InterviewerListItem
               key={interviewer.id}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,19 +3,12 @@ import classNames from "classnames";
 import "./InterviewListItem.scss";
 
 export default function InterviewerListItem(props) {
-
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
-  })
+  });
 
-  
   return (
-    <li 
-    className={interviewerClass} 
-    key={props.id}
-    selected={props.selected}
-    onClick={props.setInterviewer} 
-    >
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
